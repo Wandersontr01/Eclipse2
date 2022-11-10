ca0d60724958658d4794cfa17b94ec1debde1e32
Wandersontr01 <wanderson.franca@aluno.faesa.br>
2022-11-10 17:09:42 -0300

    Terceira alteração

--- GitDesktop/src/Teste1/PrimeiraClass.java
+++ GitDesktop/src/Teste1/PrimeiraClass.java
@@ -5,6 +5,7 @@ public class PrimeiraClass {
 	public static void main(String[] args) {
 		System.out.println("primeira alteração!");
 		System.out.println("Segunda alteração");
+		System.out.println("Terceira alteração");
 	}
 
 }

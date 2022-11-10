9126cbac289c37676253c03fd9ec00ebfd70e05e
Wandersontr01 <wanderson.franca@aluno.faesa.br>
2022-11-10 17:17:05 -0300

    Classe aluno e printa matricula

--- GitDesktop/src/Teste1/PrimeiraClass.java
+++ GitDesktop/src/Teste1/PrimeiraClass.java
@@ -3,10 +3,14 @@ package Teste1;
 public class PrimeiraClass {
 
 	public static void main(String[] args) {
+		Aluno aluno = new Aluno();
+		aluno.setMatricula(1000);
+		
 		System.out.println("primeira alteração!");
 		System.out.println("Segunda alteração");
 		System.out.println("Terceira alteração");
 		System.out.println("Quarta alteração");
+		System.out.println(aluno.getMatricula());
 	}
 
 }
